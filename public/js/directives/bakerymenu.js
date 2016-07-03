b5afb73f252b4d2ccbe3d90537f1d1805c568b91
Jaeeun Lee <jaeeun.studio@gmail.com>
2016-07-03 02:54:12 -0400

    search result layout

--- public/js/directives/bakerymenu.js
+++ public/js/directives/bakerymenu.js
@@ -10,3 +10,44 @@ angular.module('bakeryMenuApp')
     }
 });
 
+angular.module('bakeryMenuApp')
+.directive('list', function () {
+    return {
+    	restrict: 'A',
+        link: function(scope, element, attrs) {
+		     scope.$watch(
+                function () { return element[0].querySelectorAll('.menu-item').length },
+                function(newValue, oldValue) {
+                    var activeTab = document.querySelector('category-active');
+                    if(newValue != oldValue) {
+                        element.parent().parent().toggleClass('searched')
+                        if(element[0].querySelectorAll('.menu-item').length === 0) {
+                           element.parent().parent().addClass('empty-section')
+                        } else {
+                            element.parent().parent().removeClass('empty-section')
+                        }
+                    }  
+                }              
+            )
+        },
+    }
+});
+
+angular.module('bakeryMenuApp')
+.directive('tab', function () {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+             scope.$watch(
+                function () { return element[0].attributes[1].nodeValue },
+                function(newValue, oldValue) {
+                    var activeTab = document.querySelector('category-active');
+                    if(newValue != oldValue) {
+                         console.log(element[0].attributes[1].nodeValue)
+                         var categoryTabSelected = document.querySelectorAll('content-category');
+                    }  
+                }              
+            )
+        },
+    }
+});
